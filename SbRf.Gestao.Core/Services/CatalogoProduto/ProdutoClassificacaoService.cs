5685d2c571d59f1d890c46c9dc6842f11788fb48
Rafael Saboia <r-leafar@hotmail.com>
2025-09-27 23:47:06 -0300

    Criação de Service para o ProdutoClassificacao.

---
+++ SbRf.Gestao.Core/Services/CatalogoProduto/ProdutoClassificacaoService.cs
@@ -0,0 +1,17 @@
+﻿using SbRf.Gestao.Core.Entities.CatalogoProduto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbRf.Gestao.Core.Services.CatalogoProduto
+{
+    interface ProdutoClassificacaoService
+    {
+        public Task<bool> ExistePaiAsync(ProdutoClassificacao entidade);
+
+
+
+    }
+}
