986828574617d41d6ba0285bca617faeed17ad1e
Rafael Saboia <r-leafar@hotmail.com>
2025-09-27 23:45:54 -0300

    Ajustado estrutura da classe.

--- SbRf.Gestao.Core/Entities/CatalogoProduto/ProdutoClassificacao.cs
+++ SbRf.Gestao.Core/Entities/CatalogoProduto/ProdutoClassificacao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SbRf.Gestao.Core.Shared.Ensure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,54 @@ namespace SbRf.Gestao.Core.Entities.CatalogoProduto
 {
     public class ProdutoClassificacao
     {
-        public string Id { get; set; }
-        public string Nome { get; set; }
+        private string _id;
+        private string _nome;
+
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+               Ensure.NotNullOrWhiteSpace(value, nameof(Id));
+               ValidarId(value);
+               _id = value;
+            }
+        }
+        public required string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                Ensure.NotNullOrWhiteSpace(value, nameof(Nome));
+                _nome = value;
+            }
+        }
+
+        private void ValidarId(string id)
+        {
+            string[] partes = id.Split('.');
+
+            if (partes.Length != 4)
+            {
+                throw new ArgumentException("A classificação precisa ter 4 níveis", nameof(Id));
+            }
+
+            foreach (string parte in partes)
+            {
+                // Verifica se há pontos duplos, no início ou no fim (resultando em parte vazia)
+                if (string.IsNullOrEmpty(parte))
+                {
+                    throw new ArgumentException("Classificação deve estar no formato X.X.X.X (ex: 1.1.0.0)", nameof(Id));
+                }
+
+                // Verifica se a parte é um número inteiro válido
+                if (!int.TryParse(parte, out _))
+                {
+                    throw new ArgumentException("Foi informado um valor que não é um número.", nameof(Id));
+                }
+            }
+
+        }
+
     }
 }
