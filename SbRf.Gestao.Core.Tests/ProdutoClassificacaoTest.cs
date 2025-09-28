839c68046a84bbae0171ad7c94e059baf222e490
Rafael Saboia <r-leafar@hotmail.com>
2025-09-27 23:47:31 -0300

    Criação de projeto de testes.

--- SbRf.Gestao.Core.Tests/ProdutoClassificacaoTest.cs
+++ SbRf.Gestao.Core.Tests/ProdutoClassificacaoTest.cs
@@ -5,15 +5,40 @@ namespace SbRf.Gestao.Core.Tests
     public class ProdutoClassificacaoTest
     {
         [Fact]
-        public void ClassificaoComUmNivel()
+        public void ClassificaoComMenosDeQuatroNiveisLancaExcessao()
         {
-            var classificacao = new ProdutoClassificacao
-            {
-                Id = "1",
-                Nome = "Eletrônicos"
-            };
+            var exception = Assert.Throws<ArgumentException>(() => new ProdutoClassificacao() { Id = "1.1", Nome = "Setor" });
 
-            Assert.False(classificacao.Id.Contains('.'));
+            Assert.Equal("A classificação precisa ter 4 níveis (Parameter 'Id')", exception.Message);
+        }
+        [Fact]
+        public void ClassificacaoLancaExcecaoQuandoIdEhNullOuVazia()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new ProdutoClassificacao() { Id = "",Nome="Setor" });
+
+            Assert.Equal("O valor não pode ser vazio. (Parameter 'Id')", exception.Message);
+        }
+
+        [Fact]
+        public void ClassificacaoLancaExcecaoQuandoNomeEhNullOuVazia()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new ProdutoClassificacao() { Id = "1.0.0.0", Nome = "" });
+
+            Assert.Equal("O valor não pode ser vazio. (Parameter 'Nome')", exception.Message);
+        }
+        [Fact]
+        public void ClassificacaoLancaExcecaoQuandoFormatoEhInvalido()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new ProdutoClassificacao() { Id = "1.2.3.", Nome = "Setor" });
+
+            Assert.Equal("Classificação deve estar no formato X.X.X.X (ex: 1.1.0.0) (Parameter 'Id')", exception.Message);
+        }
+        [Fact]
+        public void ClassificacaoLancaExcecaoQuandoFormatoLetra()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new ProdutoClassificacao() { Id = "1.2.3.A", Nome = "Setor" });
+
+            Assert.Equal("Foi informado um valor que não é um número. (Parameter 'Id')", exception.Message);
         }
     }
 }
