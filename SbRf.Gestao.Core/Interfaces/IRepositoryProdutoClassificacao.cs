63a8ccea553e268a9e874087dfc3466ed1f8050a
Rafael Saboia <r-leafar@hotmail.com>
2025-09-27 23:46:07 -0300

    criação de repositorio.

---
+++ SbRf.Gestao.Core/Interfaces/IRepositoryProdutoClassificacao.cs
@@ -0,0 +1,20 @@
+﻿using SbRf.Gestao.Core.Entities.CatalogoProduto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbRf.Gestao.Core.Interfaces
+{
+    interface IRepositoryProdutoClassificacao
+    {
+        public  Task<ProdutoClassificacao> CriarAsync(ProdutoClassificacao entidade);
+
+        public Task<ProdutoClassificacao?> ObterPorIdAsync(string id);
+
+        public Task AtualizarAsync(ProdutoClassificacao entidade);
+
+        public Task ApagarAsync(ProdutoClassificacao entidade);
+    }
+}
