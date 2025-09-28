677c2c3223c1e1e8d98af92da2e87c0161cffe16
Rafael Saboia <r-leafar@hotmail.com>
2025-09-27 22:19:45 -0300

    criado nova entidade

---
+++ SbRf.Gestao.Core/Entities/CatalogoProduto/ProdutoClassificacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbRf.Gestao.Core.Entities.CatalogoProduto
+{
+    public class ProdutoClassificacao
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
