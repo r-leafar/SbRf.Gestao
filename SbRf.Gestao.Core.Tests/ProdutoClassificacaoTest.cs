18e497031e79435e9ab623222cec16f52db618c2
Rafael Saboia <r-leafar@hotmail.com>
2025-09-27 22:20:05 -0300

    criado projeto de teste

---
+++ SbRf.Gestao.Core.Tests/ProdutoClassificacaoTest.cs
@@ -0,0 +1,19 @@
+﻿using SbRf.Gestao.Core.Entities.CatalogoProduto;
+
+namespace SbRf.Gestao.Core.Tests
+{
+    public class ProdutoClassificacaoTest
+    {
+        [Fact]
+        public void ClassificaoComUmNivel()
+        {
+            var classificacao = new ProdutoClassificacao
+            {
+                Id = "1",
+                Nome = "Eletrônicos"
+            };
+
+            Assert.False(classificacao.Id.Contains('.'));
+        }
+    }
+}
